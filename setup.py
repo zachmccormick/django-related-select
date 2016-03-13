e15d647af0c075144464d9ae288b27d5f2d26e8c
Zach McCormick <zach.mccormick@smilecareclub.com>
2016-03-13 03:09:11 -0500

    Still learning PyPi :-)  This bump should allow me to upload the version successfully

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='django-related-select',
     packages=['related_select'],
-    version='0.2',
+    version='0.3',
     description='Class-based View and django form field for related select boxes',
     author='Zach McCormick',
     author_email='zach.mccormick@smilecareclub.com',
     url='https://github.com/CamelotVG/django-related-select',
-    download_url='https://github.com/CamelotVG/django-related-select/tarball/0.2',
+    download_url='https://github.com/CamelotVG/django-related-select/tarball/0.3',
     keywords=['django', 'related', 'dependent', 'forms'],
     classifiers=[],
     install_requires=[
