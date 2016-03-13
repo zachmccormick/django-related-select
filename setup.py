04e2f40a2ce366df02bf9c7de743dd7716e0b19f
Zach McCormick <zach.mccormick@smilecareclub.com>
2016-03-13 01:55:34 -0600

    Fixing setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='django-related-select',
-    packages=['related_select'],  # this must be the same as the name above
-    version='0.1',
+    packages=['related_select'],
+    version='0.2',
     description='Class-based View and django form field for related select boxes',
     author='Zach McCormick',
     author_email='zach.mccormick@smilecareclub.com',
-    url='https://github.com/CamelotVG/django-related-field',  # use the URL to the github repo
-    download_url='https://github.com/CamelotVG/django-related-field/tarball/0.1',
+    url='https://github.com/CamelotVG/django-related-select',
+    download_url='https://github.com/CamelotVG/django-related-select/tarball/0.2',
     keywords=['django', 'related', 'dependent', 'forms'],
     classifiers=[],
     install_requires=[
